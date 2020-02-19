9459a46d3b6e54bc53c8f70b9f4bd6de5b51a4df
Danj-o <dinodelishers@yahoo.com>
2020-02-19 16:04:24 -0500

    clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,6 @@ const IndexPage = () =>{
         <Head title="home"/>
         <h1> Hello!</h1>
         <h2>im a guy who makes stuff</h2>
-        <p>Need a dev? <Link to="/contact">Contact Me!</Link></p>
       </Layout>
     </div>
   )
